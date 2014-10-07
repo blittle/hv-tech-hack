e671281bf520fa2c2e224a4158e389141039eb21
Dave Smith <dsmith@hirevue.com>
2014-10-07 13:15:40 -0600

    Streaming from twitter (firehose)

--- vote_taker/vote_taker.js
+++ vote_taker/vote_taker.js
@@ -1,3 +1,12 @@
+var util = require("util"),
+    twitter = require("twitter"),
+    twitter_auth = require("./twitter_auth");
 
+var twit = new twitter(twitter_auth.auth);
 
-console.log("This is a test!");
+console.log("Connecting to Twitter stream...");
+twit.stream('statuses/sample', function(stream) {
+    stream.on('data', function(data) {
+        console.log(util.inspect(data));
+    });
+});
