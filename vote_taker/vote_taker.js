5fab40b967b314203c43f69219a6cde2ab6750f1
nathan <nathannospam@gmail.com>
2014-10-07 19:15:29 -0600

    change vote options to byu/uofu

--- vote_taker/vote_taker.js
+++ vote_taker/vote_taker.js
@@ -7,7 +7,7 @@ var util = require("util"),
     tallies,
     twitter_stream,
     database = new firebase("https://crackling-fire-2734.firebaseio.com/"),
-    initial_tallies = {red: 0, green: 0, blue: 0};
+    initial_tallies = {nan: 0, uofu: 0, byu: 0};
 
 process.on('exit', function(code) {
     if (twitter_stream) {
